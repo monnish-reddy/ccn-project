6f1d393fe41112388d85fd88dc250e4edca5fa03
Monnish <monnish@Monnishs-Air.lan>
2025-12-01 22:57:41 -0500

    Initial RoboShop project structure added

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0b8550f464d4056aa" # replace with your SG ID
+SG_ID="sg-0679596d3e984b23d" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z02627869X7KG52VMCNF" # replace with your ZONE ID
 DOMAIN_NAME="monnish.com" # replace with your domain
@@ -9,7 +9,7 @@ DOMAIN_NAME="monnish.com" # replace with your domain
 for instance in ${INSTANCES[@]}
 #for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0b8550f464d4056aa --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0679596d3e984b23d --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
